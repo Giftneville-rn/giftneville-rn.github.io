b317d62b5c938c36ac062d3c4ca3d608fda12510
Giftneville Onyango <onyangogift@gmail.com>
2026-07-31 15:25:37 +0300

    Portfolio Version 2 - Professional redesign

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Website loaded successfully!");
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+sections.forEach((section) => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
